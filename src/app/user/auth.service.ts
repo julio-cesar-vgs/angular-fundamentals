31f9d5183a0c44d637dec5df6443e4454ec1a4e0
Julio Souza <soneka1234@hotmail.com>
2021-06-29 13:58:53 -0300

    Capitulo 7 - Collecting Data With Angular Form and Validation.
    Aula 6 - Create your first react form

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,25 @@
+import {Inject, Injectable} from '@angular/core';
+import {IUser} from './user.model';
+
+@Injectable()
+export class AuthService {
+  currentUser: IUser;
+
+
+  loginUser(userName: string, password: string): void {
+    this.currentUser = {
+      id: 1,
+      userName,
+      firstName: 'Julio',
+      lastname: 'Souza',
+    };
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.currentUser;
+  }
+  updateCurrentUser(firstName, lastName: string): void {
+    this.currentUser.lastname = lastName;
+    this.currentUser.firstName = firstName;
+  }
+}
